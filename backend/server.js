23be2c1a1049928f4001007148aa4bad33173067
Jory Schmidt <joryschmidt1988@gmail.com>
2018-12-11 22:35:08 +0000

    write server file, routes, and issue model

---
+++ backend/server.js
@@ -0,0 +1,71 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+
+import Issue from './models/Issue';
+
+const app = express();
+const router = express.Router();
+
+// MIDDLEWARE
+app.use(cors());
+app.use(bodyParser.json());
+
+// DATABASE
+mongoose.connect(`mongodb://${process.env.IP}:27017/issues`, { useNewUrlParser: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log('Mongo connection established');
+});
+
+// Get all issues
+router.route('/issues').get((req, res) => {
+  Issue.find((err, issues) => {
+    if (err) console.log(err);
+    else res.json(issues);
+  });
+});
+
+// Find specific issue by id
+router.route('/issues/:id').get((req, res) => {
+  Issue.findById(req.params.id, (err, issue) => {
+    if (err) console.log(err);
+    else res.json(issue);
+  });
+});
+
+// Add issue to database
+router.route('/issues/add').post((req, res) => {
+  let issue = new Issue(req.body);
+  issue.save().then(issue => res.status(200).json({ 'issue': 'Added successfully' })).catch(err => res.status(400).send('Failed to create new issue'));
+});
+
+// Update existing issue
+router.route('/issues/update/:id').put((req, res, next) => {
+  Issue.findById(req.params.id, (err, issue) => {
+    if (!issue) next(new Error('Could not load document'));
+    else {
+      issue.title = req.body.title;
+      issue.responsible = req.body.responsible;
+      issue.description = req.body.description;
+      issue.severity = req.body.severity;
+      issue.status = req.body.status;
+      
+      issue.save().then(issue => res.status(200).send('Update done')).catch(err => res.status(500).send('Update failed'));
+    }
+  });
+});
+
+// Delete an issue
+router.route('/issues/delete/:id').get((req, res) => {
+  Issue.findByIdAndRemove({ _id: req.params.id }, (err, issue) => {
+    if (err) res.status(500).send(err);
+    else res.json(issue);
+  }); 
+});
+
+app.use('/', router);
+
+const port = 8080;
+app.listen(port, () => console.log(`Express server listening on port ${port}`));
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MatToolbarModule } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material';
+
+import { IssueService } from './issue.service';
 
 @NgModule({
   declarations: [
@@ -14,9 +18,10 @@ import { MatToolbarModule } from '@angular/material';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [IssueService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
